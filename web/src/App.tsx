a471cf8f09aaa4605f71b94eabb46bfb9a919ccf
itsjon4s <motajoao670@gmail.com>
2022-09-12 18:20:55 +0100

    feat: base building

---
+++ web/src/App.tsx
@@ -0,0 +1,20 @@
+interface ButtonProps {
+  title: string;
+}
+
+const Button = (props: ButtonProps) => {
+  return <button>{props.title}</button>;
+};
+
+function App() {
+  return (
+    <div>
+      <Button title="send 1" />
+      <Button title="send 2" />
+      <Button title="send 3" />
+      <Button title="send 4" />
+    </div>
+  );
+}
+
+export default App;
